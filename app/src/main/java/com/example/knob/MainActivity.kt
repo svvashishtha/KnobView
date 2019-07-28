22be0deb62f9b8c28c9a77baa1baedb3b93cf8cb
svvashishtha <saurabh.vashisht@zomato.com>
2019-07-28 23:19:56 +0530

    first commit

---
+++ app/src/main/java/com/example/knob/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.knob
+
+import android.animation.ValueAnimator
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.animation.DecelerateInterpolator
+import android.animation.ObjectAnimator
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
